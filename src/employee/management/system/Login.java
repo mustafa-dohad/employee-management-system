ea795d5ee899062b68732ff7f462de456532523d
Mustafa Dohad <mustafamurtazadohad@gmail.com>
2024-06-19 13:57:55 +0500

    [core] add class 'Main_class', did some tweaks on Login.java and connected mysql file with jdbc connector in conn.java

--- src/employee/management/system/Login.java
+++ src/employee/management/system/Login.java
@@ -1,95 +1,106 @@
 package employee.management.system;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 
 public class Login extends JFrame implements ActionListener {
     JTextField tusername;
     JPasswordField tpassword;
 
-    JButton login,back;
+    JButton login, back;
 
-
-    Login()
-    {
+    Login() {
         setLayout(null);
 
         JLabel username = new JLabel("Username");
-        username.setBounds(40,20,100,30);
+        username.setBounds(40, 20, 100, 30);
         add(username);
 
         tusername = new JTextField();
-        tusername.setBounds(150,20,150,30);
+        tusername.setBounds(150, 20, 150, 30);
         add(tusername);
 
         JLabel password = new JLabel("Password");
-        password.setBounds(40,70,100,30);
+        password.setBounds(40, 70, 100, 30);
         add(password);
 
         tpassword = new JPasswordField();
-        tpassword.setBounds(150,70,150,30);
+        tpassword.setBounds(150, 70, 150, 30);
         add(tpassword);
 
         login = new JButton("LOGIN");
-        login.setBounds(150,140,150,30);
+        login.setBounds(150, 140, 150, 30);
         login.setBackground(Color.gray);
         login.setForeground(Color.black);
         login.addActionListener(this);
         add(login);
 
-
         back = new JButton("BACK");
-        back.setBounds(150,180,150,30);
+        back.setBounds(150, 180, 150, 30);
         back.setBackground(Color.gray);
         back.setForeground(Color.black);
         back.addActionListener(this);
         add(back);
 
-        ImageIcon i11=new ImageIcon(ClassLoader.getSystemResource("icons/second.jpg"));
-        Image i22=i11.getImage().getScaledInstance(600,400,Image.SCALE_DEFAULT);
-        ImageIcon i33=new ImageIcon(i22);
+        ImageIcon i11 = new ImageIcon(ClassLoader.getSystemResource("icons/second.jpg"));
+        Image i22 = i11.getImage().getScaledInstance(600, 400, Image.SCALE_DEFAULT);
+        ImageIcon i33 = new ImageIcon(i22);
         JLabel imgg = new JLabel(i33);
-        imgg.setBounds(350,10,600,400);
+        imgg.setBounds(350, 10, 600, 400);
         add(imgg);
 
-        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icons/LoginB.jpg"));
-        Image i2=i1.getImage().getScaledInstance(600,300,Image.SCALE_DEFAULT);
-        ImageIcon i3=new ImageIcon(i2);
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/LoginB.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(600, 300, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
         JLabel img = new JLabel(i3);
-        img.setBounds(0,0,600,300);
+        img.setBounds(0, 0, 600, 300);
         add(img);
 
-
-
         setSize(600, 300);
-        setLocation(450,200);
+        setLocation(450, 200);
         setVisible(true);
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==login){
+        if (e.getSource() == login) {
+            try {
+                String username = tusername.getText();
+                String password = new String(tpassword.getPassword());
 
-        }
-        else if (e.getSource()==back) {
+                conn conn = new conn();
+                String query = "select * from login where username= '" + username + "' and password= '" + password + "'";
+
+                ResultSet resultSet = conn.statement.executeQuery(query);
+                if (resultSet.next()) {
+                    setVisible(false);
+                    new Main_class();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Invalid Username or Password");
+                }
+
+            } catch (Exception E) {
+                E.printStackTrace();
+            }
+        } else if (e.getSource() == back) {
             System.exit(0);
         }
     }
 
-    public static void main(String[] args) {        
+    public static void main(String[] args) {
         try {
-        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-            if ("Nimbus".equals(info.getName())) {
-                UIManager.setLookAndFeel(info.getClassName());
-                break;
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-    } catch (Exception e) {
-        // If Nimbus is not available, you can set the GUI to another look and feel.
-        e.printStackTrace();
-    }
 
         new Login();
     }
